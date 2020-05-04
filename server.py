134510e6d111de6677a9ed1484726513ade37a26
masashi yoshikawa <earnest.marshi@gmail.com>
2020-05-04 14:40:35 +0900

    read file api

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
+import os
+
 import flask
 import numpy as np
+import glob
+import toml
 
 question_list = [
     "import numpy as np",
@@ -16,3 +20,30 @@ def index():
 @app.route('/api/question')
 def question():
     return flask.jsonify({'question': np.random.choice(question_list)})
+
+@app.route('/api/files')
+def files():
+    directory = flask.request.args.get('directory', '')
+    if directory == "":
+        directory = "."
+    
+    files_dict = {}
+    for path in glob.glob("{}/*".format(directory)):
+        files_dict[path] = os.path.isdir(path)
+    return flask.jsonify(files_dict)
+
+@app.route('/api/read_file')
+def read_file():
+    path = flask.request.args.get('path', '')
+    if path == "":
+        return flask.jsonify({"result": "err"})
+
+    try:
+        dict_toml = toml.load(open(path))
+    except:
+        return flask.jsonify({"result": "err"})
+    
+    return flask.jsonify({
+        "result": "ok",
+        "toml": dict_toml
+    })
